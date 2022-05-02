20a68a200d014844398813f7d0b3077a2446768b
karima <timmermann@gmx.at>
2022-05-02 10:54:15 +0200

    Add function handlink clicks to cards

--- src/Components/card-container.js
+++ src/Components/card-container.js
@@ -6,10 +6,23 @@ const CardContainer = () => {
   const [allCardDB, setAllCardDB] = useState( generateCardData() );  
   
 
+  const handleclick = (event) => {
+    let newDatabase = JSON.parse(JSON.stringify(allCardDB));
+    const card = allCardDB.findIndex(element => element.id === event.target.className);
+    if (allCardDB[card].selected) {
+      console.log("OOps");
+    } else {
+      newDatabase[card].selected = true;
+      setAllCardDB(newDatabase);
+      console.log("Well done");
+    }
+  };
+
+
   const displayCards = () => {
     let tmp = [];
     for (let i=0; i<23; i++){
-      tmp.push(<img src = { allCardDB[i].name } key={ allCardDB[i].id } /> );  
+      tmp.push(<img src = { allCardDB[i].name } key={ allCardDB[i].id } onClick={ handleclick } className={ allCardDB[i].id }/> );  
     } 
      return tmp; 
   };
